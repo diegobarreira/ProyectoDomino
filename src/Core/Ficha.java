50d559ca1af8134276744744d4e20a3c9b406472
diegobarreira <diegobarreirabarrei@gmail.com>
2017-04-28 13:53:59 +0200

    Version 2 Version final, lista para entregar

--- src/Core/Ficha.java
+++ src/Core/Ficha.java
@@ -10,19 +10,13 @@ public class Ficha {
     private int derecha;
     private int izquierda;
     private int elemento;
-
-    private Ficha siguiente;
-
+   
     public Ficha(int derecha, int izquierda) {
         this.derecha = derecha;
         this.izquierda = izquierda;
     }
 
     public Ficha() {
-    }
-
-    Ficha(int MIN_VALUE, Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getDerecha() {
@@ -45,16 +39,9 @@ public class Ficha {
         return elemento;
     }
 
-    public Ficha getSig() {
-        return siguiente;
-    }
 
     public void setElemento(int elem) {
         elemento = elem;
-    }
-
-    public void setSig(Ficha sig) {
-        siguiente = sig;
     }
 
     @Override
